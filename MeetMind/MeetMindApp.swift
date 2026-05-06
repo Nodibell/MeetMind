70869b1752efaf98565b12374c968f35c8cf3d48
Oleksii Chumak <oleksiichumak@icloud.com>
2026-05-06 16:40:27 +0300

    Initial Commit

---
+++ MeetMind/MeetMindApp.swift
@@ -0,0 +1,32 @@
+//
+//  MeetMindApp.swift
+//  MeetMind
+//
+//  Created by Oleksii Chumak on 06.05.2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MeetMindApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
